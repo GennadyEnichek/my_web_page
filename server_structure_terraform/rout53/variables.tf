d793be22627e8ffea5c821848d7d74f6eabc9228
Gennady Enichek <gennady.enichek@gmail.com>
2024-05-16 15:27:17 +0300

    Update create_server_structure.yum file

--- server_structure_terraform/rout53/variables.tf
+++ server_structure_terraform/rout53/variables.tf
@@ -1,17 +1,17 @@
 variable "hosted-zone-name" {
   description = "The name of the AWS hosted zone"
   type        = string
-  default     = "gj.eu"
+  default     = "gj85.eu"
 }
 
 variable "host-record-name" {
   description = "Record for host domain"
   type        = string
-  default     = "gj.eu"
+  default     = "gj85.eu"
 }
 
 variable "www-record-name" {
   description = "Record for www domain"
   type        = string
-  default     = "www.gj.eu"
+  default     = "www.gj85.eu"
 }
